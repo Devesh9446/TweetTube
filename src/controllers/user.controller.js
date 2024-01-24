2869ec8d41b35ed2925bd9b68a26a4bcc93253d7
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-01-24 05:58:16 +0530

    Jwt setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import {apiResponse} from "../utils/apiResponse.js"
 import {user} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
+const generateAccessTokenRefreshToken=async(userId)=>{
+    try{
+        const User=user.findById(userId)
+        const accessToken=await User.generateAccessToken
+        const refreshToken=await User.generateRefreshToken
+        User.refreshToken=refreshToken
+        await user.save({validateBeforeSave:false})//as now if we save password and other also required filed so to remove validation
+    
+        return {accessToken,refreshToken}
+
+    }catch(errror){
+        throw new apiError(400,"something went wrong in creating tokens")
+    }
+}
+
 const registerUser =asyncHandler( async(req,res)=>{
     // console.log("reached:")
     // res.status(200).json({
@@ -85,4 +100,77 @@ const registerUser =asyncHandler( async(req,res)=>{
     )
 })
 
-export {registerUser}
+const logginUser =asyncHandler( async(req,res)=>{
+    //req body->data
+    //username or email
+    //find the user
+    //password check
+    //access and refresh token
+    //send cookie
+    const {email,username,password}=req.body;
+    if(!email || !username)
+    {
+        throw new apiError(400,"email or username required")
+    }
+    const User=await user.findOne({
+        $or:[{email},{username}]
+    })
+    if(!User)
+    {
+        throw new apiError(404,"Invalid Email Id or Password")
+    }
+    const isPasswordCorrect=await user.isPasswordCorrect(password)
+    if(!isPasswordCorrect)
+    {
+        throw new apiError(401,"Invalid User Credentials")
+    }
+
+    const{accessToken,refreshToken}=await generateAccessTokenRefreshToken(User._id)
+
+    const loggedInUser=await user.findById(User._id).select("-password -refreshToken")
+
+    const options={       //now cookie can be modified by server only not by frontend
+        htttpOnly :true,
+        secure:true
+    }
+    return res.status(200)
+    .cookie("refreshToken",refreshToken,options)
+    .cookie("accessToken",accessToken,options)
+    .json(//if it is an some application not web one then tokens store in json format
+        new apiResponse(
+            200,
+            {
+                user:loggedInUser,accessToken,refreshToken
+            },
+            "User LoggedIn Successfully"
+        )
+    )
+})
+
+const logOutUser=asyncHandler(async(req,res)=>{
+    user.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true     //now sending res it did not show refreshToken
+        }
+    )
+    const options={       //now cookie can be modified by server only not by frontend
+        htttpOnly :true,
+        secure:true
+    }
+    return res.status(200)
+    .clearcookie("refreshToken",options)
+    .clearcookie("accessToken",options)
+    .json(new apiResponse(200,{},"User LogOut Successfully"))
+})
+
+export {
+    registerUser,
+    logginUser,
+    logOutUser
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 import {Router} from 'express'
 import{registerUser} from "../controllers/user.controller.js"
+import{logginUser} from "../controllers/user.controller.js"
+import{logOutUser} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router =Router()
 
@@ -16,5 +19,9 @@ router.route("/register").post(
         }
     ])
 ,registerUser)//as /user call  it gives control to register and register call controller  registerUser
+
+router.router("/logginUser").post(logginUser)
+router.router("/logginUser").post(logginUser)
+router.router("/logOutUser").post(verifyJWT,logOutUser)
 
 export default router
